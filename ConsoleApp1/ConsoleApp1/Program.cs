57259b7c26e74d2b0b02126789598997834946e1
rumsontrader <30671883+rumsontrader@users.noreply.github.com>
2017-08-03 18:45:16 -0400

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Added some code here
         }
     }
 }
