fa1b21c1ad14413818d7861ef21e6f846c1087ab
Jim Anderson <rumsontrader@gmail.com>
2017-08-03 18:50:07 -0400

    edited code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // Code was edited in Github
+            //Code was added in visual studio
         }
     }
 }
